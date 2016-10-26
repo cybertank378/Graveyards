ed1183ed03ad3aa09002f008c5c7dbacb57f4b49
cybertank378 <cybertank378@gmail.com>
2016-10-26 13:07:05 +0700

    refactor  structur program and methode

---
+++ app/src/main/java/ubay/id/app/AppConfig.java
@@ -0,0 +1,16 @@
+package ubay.id.app;
+
+/**
+ * Created              : Rahman on 10/26/2016.
+ * Date Created         : 10/26/2016 / 10:29 AM.
+ * ===================================================
+ * Package              : ubay.id.app.
+ * Project Name         : graveyard.
+ * Copyright            : Copyright @ 2016 Indogamers.
+ */
+public class AppConfig  {
+    public static String HOST_URL  = "http://makamkeramatpucung.com/api/";
+
+    //Post URL Method
+    public static String URL_LOGIN = HOST_URL + "login";
+}
